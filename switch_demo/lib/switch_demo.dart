4706575c8a9802b8fb1b4d2a30ecab04ed5f2dd1
VPrasanna06 <pprasanna0156@gmail.com>
2025-03-19 21:40:49 +0530

    Add files via upload

---
+++ switch_demo/lib/switch_demo.dart
@@ -0,0 +1,15 @@
+void sdemo(String fruit)
+{
+  switch(fruit)
+  {
+    case 'apple'||'APPLE':
+      print("Apple");
+    case 'mango'||'MANGO':
+      print("Mango");
+    case 'guava'||'GUAVA':
+      print("Guava");
+      break;
+    default:
+      print("The friut is not listed");
+  }
+}
